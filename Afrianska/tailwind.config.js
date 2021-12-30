2297b1dcb7a4744d23ab1f49474daab0562a57b6
vinhtieng <vinhtieng123@gmail.com>
2021-12-30 14:35:46 +0700

    Done afrianska

--- Afrianska/tailwind.config.js
+++ Afrianska/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue1f: '#1F3F68',
+        blue5a: '#5A98D0',
+        blue31: '#316099'
+      }
+    },
   },
   plugins: [],
 }
